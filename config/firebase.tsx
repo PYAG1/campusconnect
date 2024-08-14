75bc994a56b3cce4a67c4b2e2edddb87a8d28fa4
Papa Yaw Agyeman-Gyekye. <96929032+PYAG1@users.noreply.github.com>
2024-08-14 13:05:36 +0000

    feat: Change in firebase config

--- config/firebase.tsx
+++ config/firebase.tsx
@@ -10,16 +10,16 @@ import {getStorage} from "firebase/storage"
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
-const firebaseConfig = {
-    apiKey: "AIzaSyBWS6t5OdrvEBdYZtNv7GwyTFuQk-drLl8",
-    authDomain: "campusconnect-97e95.firebaseapp.com",
-    projectId: "campusconnect-97e95",
-    storageBucket: "campusconnect-97e95.appspot.com",
-    messagingSenderId: "601831665094",
-    appId: "1:601831665094:web:c23e652e30097c0de516c7",
-    measurementId: "G-H3BPHM9KVQ"
-  };
 
+  const firebaseConfig = {
+    apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID,
+    measurementId: process.env.EXPO_PUBLIC_FIREBASE_MEASUREMENT_ID
+};
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
